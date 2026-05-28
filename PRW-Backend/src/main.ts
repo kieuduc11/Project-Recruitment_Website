640336febb405f7d269d9cf43bad265a6aed75e3
Kieu Duc <kieuduc223@gmail.com>
2026-05-29 01:07:55 +0700

    fixed cors

--- PRW-Backend/src/main.ts
+++ PRW-Backend/src/main.ts
@@ -10,15 +10,22 @@ import { JwtAuthGuard } from './auth/jwt-auth.guard';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  const configService = app.get(ConfigService);   
+  const configService = app.get(ConfigService);
 
   const reflector = app.get(Reflector);
-  app.useGlobalGuards(new JwtAuthGuard(reflector));
+  // app.useGlobalGuards(new JwtAuthGuard(reflector));
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
   app.useGlobalPipes(new ValidationPipe());
+
+  // config cors
+  app.enableCors({
+    "origin": "http://localhost:3000",
+    "methods": "GET, HEAD, PUT, PATCH, POST, DELETE",
+    "optionsSuccessStatus": 200
+  });
 
   await app.listen(configService.get<string>("PORT"));
 }
