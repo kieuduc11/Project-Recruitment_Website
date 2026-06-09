46118fd8cc52b6c2ca9ae29d88319fe79a2e4585
Kieu Duc <kieuduc223@gmail.com>
2026-06-09 09:25:24 +0700

    permission and role schema

---
+++ PRW-Backend/src/roles/dto/create-role.dto.ts
@@ -0,0 +1,19 @@
+import { IsAlpha, IsArray, IsBoolean, IsMongoId, IsNotEmpty } from "class-validator";
+import mongoose from "mongoose";
+
+export class CreateRoleDto {
+    @IsNotEmpty({message: "Name không được để trống"})
+    name: string;
+
+    @IsNotEmpty({message: "Description không được để trống"})
+    description: string;
+
+    @IsNotEmpty({message: "Is Active không được để trống"})
+    @IsBoolean({message: "Is active phải có giá trị boolean"})
+    isActive: boolean;
+
+    @IsNotEmpty({message: "Permissions không được để trống"})
+    @IsMongoId({each: true, message: "Each permission là mongo object id"})
+    @IsArray({message: "Permissions có định dạng là array"})
+    permissions: mongoose.Schema.Types.ObjectId[];
+}
