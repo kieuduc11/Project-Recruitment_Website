48decf7d17ab7c84c5db8ba477f6fc8db7fbaaea
Kieu Duc <kieuduc223@gmail.com>
2026-06-09 11:28:30 +0700

    feat: CRUD role

--- PRW-Backend/src/roles/roles.service.ts
+++ PRW-Backend/src/roles/roles.service.ts
@@ -1,26 +1,103 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
+import { IUser } from 'src/users/users.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Role, RoleDocument } from './schemas/role.schema';
+import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
+import mongoose from 'mongoose';
+import aqp from 'api-query-params';
 
 @Injectable()
 export class RolesService {
-  create(createRoleDto: CreateRoleDto) {
-    return 'This action adds a new role';
+  constructor(@InjectModel(Role.name) private roleModel: SoftDeleteModel<RoleDocument>) { }
+
+  async create(createRoleDto: CreateRoleDto, user: IUser) {
+    const { name } = createRoleDto;
+
+    const isExist = await this.roleModel.findOne({ name })
+    if (isExist) throw new BadRequestException("Name đã tồn tại")
+
+    const role = await this.roleModel.create({
+      ...createRoleDto,
+      createdBy: {
+        _id: user._id,
+        email: user.email
+      }
+    })
+    return {
+      _id: role._id,
+      createdAt: role.createdAt
+    };
   }
 
-  findAll() {
-    return `This action returns all roles`;
+  async findAll(currentPage: number, limit: number, queryString: string) {
+    const { filter, sort, population } = aqp(queryString);
+    delete filter.current;
+    delete filter.pageSize;
+
+    let offset = (currentPage - 1) * (limit);
+    let defaultLimit = limit ? limit : 10;
+
+    const totalItems = (await this.roleModel.find(filter)).length;
+    const totalPages = Math.ceil(totalItems / defaultLimit);
+
+    const result = await this.roleModel.find(filter)
+      .skip(offset)
+      .limit(defaultLimit)
+      .sort(sort as any)
+      .populate(population)
+      .exec();
+
+    return {
+      meta: {
+        current: currentPage, //trang hiện tại
+        pageSize: limit, //số lượng bản ghi đã lấy
+        pages: totalPages,  //tổng số trang với điều kiện query
+        total: totalItems // tổng số phần tử (số bản ghi)
+      },
+      result //kết quả query
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} role`;
+  async findOne(id: string) {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      throw new BadRequestException("Role id không hợp lệ")
+    }
+
+    const role = await this.roleModel.findById({ _id: id })
+
+    return role;
   }
 
-  update(id: number, updateRoleDto: UpdateRoleDto) {
-    return `This action updates a #${id} role`;
+  async update(id: string, updateRoleDto: UpdateRoleDto, user: IUser) {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      throw new BadRequestException("Role id không hợp lệ")
+    }
+
+    const result = await this.roleModel.updateOne({ _id: id }, {
+      ...updateRoleDto,
+      updatedBy: {
+        _id: user._id,
+        email: user.email
+      }
+    })
+    return result;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} role`;
+  async remove(id: string, user: IUser) {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      throw new BadRequestException("Role id không hợp lệ")
+    }
+
+    await this.roleModel.updateOne({ _id: id }, {
+      deletedBy: {
+        _id: user._id,
+        email: user.email
+      }
+    })
+
+    const result = await this.roleModel.softDelete({_id: id})
+    return result;
   }
 }
