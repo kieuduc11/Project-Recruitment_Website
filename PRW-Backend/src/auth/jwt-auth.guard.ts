2824fbba8a1c305c72b70e1aa92a99c06703eae6
Kieu Duc <kieuduc223@gmail.com>
2026-06-11 18:51:34 +0700

    fix login issue

--- PRW-Backend/src/auth/jwt-auth.guard.ts
+++ PRW-Backend/src/auth/jwt-auth.guard.ts
@@ -33,9 +33,10 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
         const targetEndpoint = request.route?.path;
 
         const permissions = user?.permissions ?? []; 
-        const isExist = permissions.find(permission => 
+        let isExist = permissions.find(permission => 
             targetMethod === permission.method && targetEndpoint === permission.apiPath)
 
+        if(targetEndpoint.startsWith("/api/v1/auth")) isExist = true;
         if(!isExist) throw new ForbiddenException("Bạn không có quyền để truy cập endpoint này");
 
         return user;
